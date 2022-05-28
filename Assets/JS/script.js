3247620e010f599c604babff69c07e9ba63ec8b4
connor-kfitz <connorkfitzsimmons@gmail.com>
2022-05-28 15:34:01 -0400

    Linked API Key in JS

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -2,4 +2,10 @@ var APIKey = "eae30589ae86d52103436bee50214263";
 var city = "Austin";
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
-fetch(queryURL);
+fetch(queryURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+      });
