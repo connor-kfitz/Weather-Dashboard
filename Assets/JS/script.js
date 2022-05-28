ba8b7a37e45be5c974df59de8bdd835e6c563b86
connor-kfitz <connorkfitzsimmons@gmail.com>
2022-05-28 16:46:21 -0400

    Added data collection from API

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,6 +1,6 @@
 var APIKey = "eae30589ae86d52103436bee50214263";
-var city = "Austin";
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var city = "Toronto";
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&current.uvi=true&appid=" + APIKey;
 
 fetch(queryURL)
     .then(function (response) {
@@ -8,4 +8,32 @@ fetch(queryURL)
     })
     .then(function (data) {
         console.log(data);
+        var temp = (data.main.temp); // Cel
+        console.log(temp);
+        var wind = (data.wind.speed); // m/s
+        console.log(wind);
+        var humidity = (data.main.humidity);
+        console.log(humidity);
+        var lat = data.coord.lat;
+        console.log(lat);
+        var long = data.coord.lon;
+        console.log(long);
+        var newQueryURL = "http://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&appid=" + APIKey;
+        fetch(newQueryURL)
+            .then(function (response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            var uvIndex = data.current.uvi;
+            console.log(uvIndex);
+        })
       });
+
+// Link search bar and button
+var searchButton = $('#searchButton');
+searchButton.on('click', function (){
+    var city = $('#searchBar').val();
+    console.log(city);
+
+})
