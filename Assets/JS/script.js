372d21b0f0f0797ad8dacbd41ebf36781dc3a4e0
connor-kfitz <connorkfitzsimmons@gmail.com>
2022-05-31 17:37:14 -0400

    Cleaned and Commeneted Code

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,28 +1,18 @@
+//Global Variables and Array Keys
 var APIKey = "eae30589ae86d52103436bee50214263";
 var boxTempDates =['#oneDayDate', '#twoDayDate', '#threeDayDate', '#fourDayDate', '#fiveDayDate'];
 var boxTempIDs =['#oneDayTemp', '#twoDayTemp', '#threeDayTemp', '#fourDayTemp', '#fiveDayTemp'];
 var boxWindIDs =['#oneDayWind', '#twoDayWind', '#threeDayWind', '#fourDayWind', '#fiveDayWind'];
 var boxHumidityIDs =['#oneDayHumidity', '#twoDayHumidity', '#threeDayHumidity', '#fourDayHumidity', '#fiveDayHumidity'];
+var boxIconIDs =['#oneDayWeatherIcon', '#twoDayWeatherIcon', '#threeDayWeatherIcon', '#fourDayWeatherIcon', '#fiveDayWeatherIcon'];
 
 var historyMatrix = ["Toronto", "Etobicoke", "Ajax", "Waterloo", "Guelph"];
-var historyMatrixCount =0;
 
 var breakFunction = false;
 
-
-function checkData(cityName) {
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?units=metric&q=" + cityName + "&current.uvi=true&appid=" + APIKey;
-    fetch(queryURL)
-        .then(function (response) {
-            if(response.status != 200) {
-                breakFunction = true;
-            }
-        })
-}
-
+//Functions
+// // Function to collect and display data
 function getData(cityName) {
-
-    var city;
     var temp;
     var wind;
     var humidity;
@@ -53,93 +43,60 @@ function getData(cityName) {
             $('#todayHumidity').text(humidity.toString());
             
             var lat = data.coord.lat;
-            console.log(lat);
             var long = data.coord.lon;
-            console.log(long);
             var newQueryURL = "http://api.openweathermap.org/data/2.5/onecall?units=metric&lat=" + lat + "&lon=" + long + "&appid=" + APIKey;
             fetch(newQueryURL)
                 .then(function (response) {
                 return response.json();
             })
-            .then(function(data) {
-                console.log(data);
-                uvIndex = data.current.uvi;
-                console.log(uvIndex);
-                iconCode = data.current.weather[0].icon;
-                console.log(iconCode);
-                for(var i=0; i < 5; i++){
-                    fiveDayIconCode[i] += data.daily[i].weather[0].icon;
-                    fiveDayTemp[i] += data.daily[i].temp.day;
-                    fiveDayWind[i] += data.daily[i].wind_speed;
-                    fiveDayHumidity[i] += data.daily[i].humidity;
+                .then(function(data) {
+                    uvIndex = data.current.uvi;
+                    iconCode = data.current.weather[0].icon;
+                    for(var i=0; i < 5; i++){
+                        fiveDayIconCode[i] += data.daily[i].weather[0].icon;
+                        fiveDayTemp[i] += data.daily[i].temp.day;
+                        fiveDayWind[i] += data.daily[i].wind_speed;
+                        fiveDayHumidity[i] += data.daily[i].humidity;
+                        }
+                    $('#weatherIcon').attr("src", "http://openweathermap.org/img/wn/" + iconCode + "@2x.png");
+                    $('#todayUV').text(uvIndex.toString());
+
+                    //Set UV Index Colour
+                    if(uvIndex < 3) {
+                        $('#todayUV').css('background', 'yellow');
+                        console.log('yes');
+                    } 
+                    else if(uvIndex < 6) {
+                        $('#todayUV').css('background', 'green');
+                    } 
+                    else {
+                        $('#todayUV').css('background', 'red');
                     }
-                $('#weatherIcon').attr("src", "http://openweathermap.org/img/wn/" + iconCode + "@2x.png");
-                $('#todayUV').text(uvIndex.toString());
-                //UV Index Colour
-                if(uvIndex < 3) {
-                    $('#todayUV').css('background', 'yellow');
-                    console.log('yes');
-                } 
-                else if(uvIndex < 6) {
-                    $('#todayUV').css('background', 'green');
-                } 
-                else {
-                    $('#todayUV').css('background', 'red');
-                }
 
-                var boxIconIDs =['#oneDayWeatherIcon', '#twoDayWeatherIcon', '#threeDayWeatherIcon', '#fourDayWeatherIcon', '#fiveDayWeatherIcon'];
-                for(var i =0; i < 5; i++){
-                    $(boxTempDates[i]).text(moment().add(i, 'days').format('MMM Do YY'))
-                    $(boxIconIDs[i]).attr("src", "http://openweathermap.org/img/wn/" + fiveDayIconCode[i] + "@2x.png");
-                    $(boxTempIDs[i]).text(fiveDayTemp[i]);
-                    $(boxWindIDs[i]).text(fiveDayWind[i]);
-                    $(boxHumidityIDs[i]).text(fiveDayHumidity[i]);
-                }
-                console.log(fiveDayIconCode);
-
-                var cityWeather = {
-                    location: city,
-                    icon: iconCode,
-                    todayTemp: temp,
-                    todayHumid: humidity,
-                    todayUV: uvIndex,
-                    fiveDayIconCode: fiveDayIconCode,
-                    fiveDayTemp: fiveDayTemp,
-                    fiveDayWind: fiveDayWind,
-                    fiveDayHumidity: fiveDayHumidity
-                }
-                console.log(cityWeather);
-                localStorage.setItem(city, JSON.stringify(cityWeather));
-            })
+                    //Display collected info on screen
+                    for(var i =0; i < 5; i++){
+                        $(boxTempDates[i]).text(moment().add(i, 'days').format('MMM Do YY'))
+                        $(boxIconIDs[i]).attr("src", "http://openweathermap.org/img/wn/" + fiveDayIconCode[i] + "@2x.png");
+                        $(boxTempIDs[i]).text(fiveDayTemp[i]);
+                        $(boxWindIDs[i]).text(fiveDayWind[i]);
+                        $(boxHumidityIDs[i]).text(fiveDayHumidity[i]);
+                    }
+                })
         });
 }
 
-getData('Toronto');
+// //Function to check for a valid city entry
+function checkData(cityName) {
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?units=metric&q=" + cityName + "&current.uvi=true&appid=" + APIKey;
+    fetch(queryURL)
+        .then(function (response) {
+            if(response.status != 200) {
+                breakFunction = true;
+            }
+        })
+}
 
-// Link search bar and button
-var searchButton = $('#searchButton');
-var searchHistory = $('.searchHistory');
-
-searchButton.on('click', function (){
-    var city = $('#searchBar').val();
-    checkData(city)
-    if(breakFunction){
-        breakFunction = false;
-        return;
-    }
-    getData(city)
-    historyMatrix.pop();
-      for(var i=0; i < 5; i++){
-          historyMatrix[4-i]=historyMatrix[3-i];
-      }
-    historyMatrix[0] = city;
-    $('#searchButton1').text(city);
-    $('#searchButton2').text(historyMatrix[1]);
-    $('#searchButton3').text(historyMatrix[2]);
-    $('#searchButton4').text(historyMatrix[3]);
-    $('#searchButton5').text(historyMatrix[4]);
-})
-
+//Set search history buttons
 var searchButton1 = $('#searchButton1');
 var searchButton2 = $('#searchButton2');
 var searchButton3 = $('#searchButton3');
@@ -165,3 +122,31 @@ getData(historyMatrix[3]);
 searchButton5.on('click', function(){
 getData(historyMatrix[4]);
 })
+
+//Display info for default city, Toronto
+getData('Toronto');
+
+//Search button event listener to run required functions
+var searchButton = $('#searchButton');
+var searchHistory = $('.searchHistory');
+
+searchButton.on('click', function (){
+    var city = $('#searchBar').val();
+    checkData(city)
+    if(breakFunction){
+        breakFunction = false;
+        return;
+    }
+    getData(city)
+    historyMatrix.pop();
+      for(var i=0; i < 5; i++){
+          historyMatrix[4-i]=historyMatrix[3-i];
+      }
+    historyMatrix[0] = city;
+    $('#searchButton1').text(city);
+    $('#searchButton2').text(historyMatrix[1]);
+    $('#searchButton3').text(historyMatrix[2]);
+    $('#searchButton4').text(historyMatrix[3]);
+    $('#searchButton5').text(historyMatrix[4]);
+})
+
---
+++ README.md
@@ -0,0 +1,14 @@
+# Server Side APIs: Weather Dashboard
+
+## Link
+
+## Description
+
+The purpose of this project is to create a weather dashboard using HTML & CSS, powered by javascript.  The dashboard is to utilize a server side API provided by Open Weather.  
+
+## Steps Taken
+
+1.
+
+## Pictures of Website
+![Picture of JavaScript Code Quiz](./Assets/Images/JavaScript-Quiz-Start-Page.png)
